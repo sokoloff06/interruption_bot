77804e1dc9efacb7c9e11266bba56fb728995d80
Vitaly <sokoloff06@gmail.com>
2019-01-29 10:26:17 +0200

    Add authentication and initial setup.

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
+// This app is only tested and designed to work with a single workspace yet
+// TODO: Make a post on the submitter's behalf
+
 const express = require('express');
 const request = require('request');
+const Store = require('data-store');
+
 const app = express();
-const appToken = "xoxp-511220587186-512622329046-522547343265-ff438c068f5ba0c1619d1baf0d994182"; //changes after the re-install
-//const userToken = "xoxp-511220587186-512622329046-522366249552-ac278479b7522e60a428cfeee38c8df2";
-const botToken = "xoxb-511220587186-512889535014-Yc1UWyFXkDjzoc5LdJ6XCPuz"; //changes after the re-install
+const store = new Store({ path: 'auth.json' })
 const client_id = "511220587186.511052758372";
 const client_secret = "b1f35600428d73ffdcb625e3e99ec59c";
-const interChannel = "CF9RR28MQ";
+
+
+// Workspace credentials and params
+var appToken;
+var botToken;
+var botUser;
+var teamName;
+var interChannel = null; //ID of the interruption channel
+
+// Accesing params from the storage file if it exists
+if (store.has('appToken')) {
+    appToken = store.get('appToken');
+    botToken = store.get('botToken');
+    botUser = store.get('botUser');
+    teamName = store.get('teamName');
+    interChannel = store.get('channel_id');
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
 // Test response for the GET request from browser
-app.get('/', (req, res) => res.send('<p>Hello World!</p>' + '<p>Recieved the following "Accept" header: </p><br>' + req.headers.accept));
+app.get('/', (req, res) => res.send('<p>Hello!</p>' + '<p>Interruption helper app is running</p><br>'));
 
 // Indication app is started
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
+// Posts a message using the following params:
+
+// text(required) - String message to post
+// channel(required) - String ID of a channel to post message to
+// isReplace - boolean flag indicating if we want new message to replace previous message
+// response_url - String unique URL that is being used if our message is sent in response to another
+// asUser - boolean flag, currently not used. For future if we add functionality to post messages as user
 function postMessage(text, channel, isReplace = false, response_url, asUser = false) {
     var token = botToken;
     var body = {
@@ -25,10 +51,10 @@ function postMessage(text, channel, isReplace = false, response_url, asUser = fa
         "text": text
     }
 
-    if(isReplace) {
+    if (isReplace) {
         body.delete_original = true;
     }
-    if(asUser) {
+    if (asUser) {
         body.as_user = false;
         token = userToken;
     }
@@ -54,8 +80,9 @@ function postMessage(text, channel, isReplace = false, response_url, asUser = fa
     });
 };
 
+// Posts confirmation message so user can preview the resulting message
 function confirmPublish(form, chnl) {
-    var text = "<!channel> Interruption alert!" + 
+    var text = "<!here>" +
         "\n*Start Time:*\n" + form.start_time +
         "\n*Cause:*\n" + form.cause +
         "\n*Impact:*\n" + form.impact +
@@ -112,90 +139,96 @@ app.post('/', (req, res) => {
     channel = body.channel_id;
     var trigger_id = body.trigger_id;
 
-    request.post('https://slack.com/api/dialog.open', {
-        json: {
-            'dialog': {
-                'callback_id': 'dialog-open',
-                'title': 'Report interruption',
-                'submit_label': 'Report',
-                'state': 'Limo',
-                'elements': [
-                    {
-                        'type': 'text',
-                        'label': 'Start Time',
-                        'name': 'start_time',
-                        'placeholder': 'e.g. March 16 2018 15:28 UTC'
-                    },
-                    // {
-                    //     'type': 'text',
-                    //     'label': 'End Time',
-                    //     'name': 'end_time',
-                    //     'placeholder': 'e.g. March 16 2018 15:28 UTC'
-                    // },
-                    {
-                        'type': 'select',
-                        'label': 'Cause',
-                        'name': 'cause',
-                        'options': [
-                            {
-                                'label': 'Issue with a vendor',
-                                'value': 'Issue with a vendor'
-                            },
-                            {
-                                'label': 'Issue with a business partner',
-                                'value': 'Issue with a business partner'
-                            },
-                            {
-                                'label': 'Internal system issue',
-                                'value': 'Internal system issue'
-                            }
-                        ]
-                    },
-                    {
-                        'type': 'textarea',
-                        'name': 'impact',
-                        'label': 'Impact',
-                        'hint': 'Which features are affected? Is there any data loss? What is the expected experience in the system a compared to the steady state?',
-                        'placeholder': 'Please write here whatever is known and be as detailed as possible. Avoid using R&D internal terminology, use feature names instead'
-                    },
-                    {
-                        'type': 'select',
-                        'label': 'Next Steps',
-                        'name': 'next_steps',
-                        'options': [
-                            {
-                                'label': 'Status update',
-                                'value': 'Next Status Update Time'
-                            },
-                            {
-                                'label': 'Resolution',
-                                'value': 'Expected Resolution Time'
-                            }
-                        ]
-                    },
-                    {
-                        'type': 'text',
-                        'label': 'Update/Resolution time',
-                        'name': 'time',
-                        'placeholder': 'e.g. March 16 2018 15:28 UTC'
-                    },
+    if (interChannel == null) {
+        res.send("Please, add ID of the service interruption channel (as channel_id) to the auth.json file of the app");
+    }
+    else {
+        res.send();
+        request.post('https://slack.com/api/dialog.open', {
+            json: {
+                'dialog': {
+                    'callback_id': 'dialog-open',
+                    'title': 'Report interruption',
+                    'submit_label': 'Report',
+                    'state': 'Limo',
+                    'elements': [
+                        {
+                            'type': 'text',
+                            'label': 'Start Time',
+                            'name': 'start_time',
+                            'placeholder': 'e.g. March 16 2018 15:28 UTC'
+                        },
+                        // {
+                        //     'type': 'text',
+                        //     'label': 'End Time',
+                        //     'name': 'end_time',
+                        //     'placeholder': 'e.g. March 16 2018 15:28 UTC'
+                        // },
+                        {
+                            'type': 'select',
+                            'label': 'Cause',
+                            'name': 'cause',
+                            'options': [
+                                {
+                                    'label': 'Issue with a vendor',
+                                    'value': 'Issue with a vendor'
+                                },
+                                {
+                                    'label': 'Issue with a business partner',
+                                    'value': 'Issue with a business partner'
+                                },
+                                {
+                                    'label': 'Internal system issue',
+                                    'value': 'Internal system issue'
+                                }
+                            ]
+                        },
+                        {
+                            'type': 'textarea',
+                            'name': 'impact',
+                            'label': 'Issue and impact',
+                            'hint': 'Please write here whatever is known and be as detailed as possible. Avoid using R&D internal terminology, use feature names instead',
+                            'placeholder': 'Which features are affected? Is there any data loss? What is the expected experience in the system a compared to the steady state?'
+                        },
+                        {
+                            'type': 'select',
+                            'label': 'Next Steps',
+                            'name': 'next_steps',
+                            'options': [
+                                {
+                                    'label': 'Status update',
+                                    'value': 'Next Status Update Time'
+                                },
+                                {
+                                    'label': 'Resolution',
+                                    'value': 'Expected Resolution Time'
+                                }
+                            ]
+                        },
+                        {
+                            'type': 'text',
+                            'label': 'Update/Resolution time',
+                            'name': 'time',
+                            'placeholder': 'e.g. March 16 2018 15:28 UTC'
+                        },
 
-                ]
+                    ]
+                },
+                'trigger_id': trigger_id
             },
-            'trigger_id': trigger_id
-        },
-        headers: {
-            'Content-type': 'application/json',
-            'Authorization': 'Bearer ' + appToken
-        }
-    });
-    res.send();
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + appToken
+            }
+        }, (error, response) => {
+            if (response.statusCode != 200) {
+                console.log(error);
+            }
+        });
+    }
 });
 
-
-// TODO: Make a post on the submitter's behalf
-
-
+// General endpoint, see comments inside
 app.post('/postReport', (req, res) => {
     var payload = JSON.parse(req.body.payload);
 
@@ -207,7 +240,7 @@ app.post('/postReport', (req, res) => {
         confirmPublish(form, payload.channel.id);
     }
 
-    // processess final interruption message publishing
+    // Processess final interruption message publishing
     else if (payload.callback_id = "publish") {
         res.send();
         if (payload.actions[0].value == "yes") {
@@ -221,9 +254,9 @@ app.post('/postReport', (req, res) => {
 
 });
 
-//OAuth
-app.get('/auth', (req, res) => {
-    console.log(req);
+// OAuth and initial setup
+app.get('/auth', (req, resp) => {
+    //console.log(req);
     var code = req.query.code;
     var url = "https://slack.com/api/oauth.access";
     var body = {
@@ -239,8 +272,23 @@ app.get('/auth', (req, res) => {
     }, (err, res, body) => {
         console.log(res);
         console.log(err);
+        var response = JSON.parse(res.body);
+        appToken = response.access_token;
+        store.set('appToken', appToken);
+        botToken = response.bot.bot_access_token;
+        store.set('botToken', botToken);
+        botUser = response.bot.bot_user_id;
+        store.set('botUser', botUser);
+        teamName = response.team_name;
+        store.set('teamName', teamName);
+        user = response.user_id;
+        store.set('admin_user', user);
+        store.set('channel_id', null);
+
+        // Redirect to slack
+        resp.redirect('https://' + teamName + '.slack.com/messages/' + botUser);
+
+        // Get interruption channel ID
+        postMessage("Please, add ID of the service interruption channel (as channel_id) to the auth.json file of the app", user);
     });
-
-    res.send();
-
 });
